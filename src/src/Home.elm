b95ac02cbe46b90dd24aa1e0f28010c6c577b0a5
Hugo Findlay <2554911f@student.gla.ac.uk>
2023-11-24 18:58:53 +0000

    added click on node to zoom in & button fns

--- src/src/Home.elm
+++ src/src/Home.elm
@@ -3,9 +3,9 @@ module Home exposing (main)
 {-
    Bored? Rotate a cube in your mind :3
 
-    7 ----- L ----- 8            
-    | K             | J
-    |   5 ----- I ----- 6
+    7 ----- L ----- 8               1 = .
+    | K             | J             2 = >
+    |   5 ----- I ----- 6           3 = <
     G   |           |   |
     |   |           H   |
     |   E           |   F
@@ -15,48 +15,52 @@ module Home exposing (main)
 -}
 
 import Browser
-import Browser.Events exposing (onAnimationFrameDelta)
-import Html exposing (Html, button, div, text)
-import Html.Attributes exposing (height, style, width)
-import Json.Decode exposing (Value)
+import Browser.Events       exposing (onAnimationFrameDelta)
+import Html                 exposing (Html, button, div, text)
+import Html.Attributes      exposing (height, style, width)
+import Json.Decode          exposing (Value)
 import Math.Matrix4 as Mat4 exposing (Mat4)
 import Math.Vector3 as Vec3 exposing (Vec3, vec3)
-import WebGL exposing (Mesh, Shader)
-import Html.Events exposing (onClick)
+import WebGL                exposing (Mesh, Shader)
+import Html.Events          exposing (onClick)
+import Browser.Dom exposing (Element)
+import Html.Attributes exposing (attribute)
 
 
 main : Program Flags Model Msg
 main =
     Browser.element
-        { init = init
-        , view = view
+        { init          = init
+        , view          = view
         , subscriptions = subscriptions
-        , update = update
+        , update        = update
         }
 
 init : Flags -> (Model, Cmd Msg )
-init _ = ( {theta = 0, eye = (vec3 -0.3  1.6 3 ), target  = (vec3 0.5 0.35 0.5)} , Cmd.none )
+init _ = ( {theta = 0, eye = (vec3 -0.3 1.6 3 ), target  = (vec3 0.5 0.35 0.5), gridOn = "diabeld", arrows = (Hid, Hid, Hid)} , Cmd.none )
 
 subscriptions : a -> Sub Msg
 subscriptions _ = onAnimationFrameDelta Tick
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model = case msg of 
-    Tick dt -> ( { model | theta = model.theta + dt / 5000}, Cmd.none )
-    SystemClicked Home   -> ( { model | eye = (vec3 -0.3 1.6 3 ), target  = (vec3 0.5 0.35 0.5)}, Cmd.none )
-    SystemClicked Simple -> ( { model | eye = (vec3 -0.2 0.15 1.3 ), target  = (vec3 0 0 1 )}, Cmd.none )
-    SystemClicked P      -> ( { model | eye = (vec3 0.8 0.15 1.3 ), target  = (vec3 1 0 1)}, Cmd.none )
-    SystemClicked Two    -> ( { model | eye = (vec3 -0.2 1.15 1.3 ), target  = (vec3 0 1 1)}, Cmd.none )
-    SystemClicked W_     -> ( { model | eye = (vec3 -0.2 0.15 0.3 ), target  = (vec3 0 0 0)}, Cmd.none )
-    SystemClicked W      -> ( { model | eye = (vec3 -0.2 1.15 0.3 ), target  = (vec3 0 1 0)}, Cmd.none )
-    SystemClicked PW_    -> ( { model | eye = (vec3 0.8 0.15 0.3 ), target  = (vec3 1 0 0)}, Cmd.none )
-    SystemClicked P2     -> ( { model | eye = (vec3 0.8 1.15 1.3 ), target  = (vec3 1 1 1)}, Cmd.none )
-    SystemClicked C      -> ( { model | eye = (vec3 0.8 1.15 0.3 ), target  = (vec3 1 1 0)}, Cmd.none )
+    Tick dt              -> ( { model | theta = model.theta + dt / 5000}, Cmd.none )
+    SystemClicked Home   -> ( { model | eye = (vec3 -0.3 1.60 3.0 ), target  = (vec3 0.5 0.35 0.5), gridOn = "dosabled", arrows = (Hid, Hid, Hid)}, Cmd.none )
+    SystemClicked Simple -> ( { model | eye = (vec3 -0.2 0.15 1.3 ), target  = (vec3 0.0 0.00 1.0), gridOn = "disabled", arrows = (Add, Add, Add)}, Cmd.none )
+    SystemClicked P      -> ( { model | eye = (vec3  0.8 0.15 1.3 ), target  = (vec3 1.0 0.00 1.0), gridOn = "disabled", arrows = (Sub, Add, Add)}, Cmd.none )
+    SystemClicked Two    -> ( { model | eye = (vec3 -0.2 1.15 1.3 ), target  = (vec3 0.0 1.00 1.0), gridOn = "disabled", arrows = (Add, Sub, Add)}, Cmd.none )
+    SystemClicked W_     -> ( { model | eye = (vec3 -0.2 0.15 0.3 ), target  = (vec3 0.0 0.00 0.0), gridOn = "disabled", arrows = (Add, Add, Sub)}, Cmd.none )
+    SystemClicked W      -> ( { model | eye = (vec3 -0.2 1.15 0.3 ), target  = (vec3 0.0 1.00 0.0), gridOn = "disabled", arrows = (Add, Sub, Sub)}, Cmd.none )
+    SystemClicked PW_    -> ( { model | eye = (vec3  0.8 0.15 0.3 ), target  = (vec3 1.0 0.00 0.0), gridOn = "disabled", arrows = (Sub, Add, Sub)}, Cmd.none )
+    SystemClicked P2     -> ( { model | eye = (vec3  0.8 1.15 1.3 ), target  = (vec3 1.0 1.00 1.0), gridOn = "disabled", arrows = (Sub, Sub, Add)}, Cmd.none )
+    SystemClicked C      -> ( { model | eye = (vec3  0.8 1.15 0.3 ), target  = (vec3 1.0 1.00 0.0), gridOn = "disabled", arrows = (Sub, Sub, Sub)}, Cmd.none )
     
 type alias Model = 
     { theta   : Float
     , eye     : Vec3
-    , target  : Vec3}
+    , target  : Vec3
+    , gridOn  : String 
+    , arrows  : (Arrow, Arrow, Arrow)}
 
 type Msg = Tick Float | SystemClicked System
 
@@ -71,24 +75,56 @@ type System =
     | P2
     | C
 
+type Arrow = Hid | Add | Sub
+    
 type alias Flags = Value
+
+button_side : System -> String -> String -> Html Msg
+button_side sys col msg = button 
+    [ onClick (SystemClicked sys)
+    , style "height" "55px"
+    , style "width" "150px"
+    , style "background-color" col
+    , style "color" "#ffffff"
+    , style "font-size" "30px"
+    , style "border" "0px"
+    , style "border-top-right-radius"  "20px"
+    , style "border-bottom-right-radius"  "20px"
+    ] [ text msg ]
+
+button_trans : System -> String -> String -> String -> Html Msg
+button_trans sys x y enabled = button 
+    [ onClick (SystemClicked sys)
+    , style "height" "75px"
+    , style "width" "75px"
+    , style "background-color" "#ff0000"
+    , style "color" "#ffffff"
+    , style "border" "0px"
+    , style "position" "absolute"
+    , style "top" y
+    , style "left" x
+    , style "opacity" "0"
+    , style "display" "flex"
+    , attribute enabled ""
+    ] [ text "" ]
 
 view : Model -> Html Msg
 view model =
     div
         [ style "font-family" "TASA Orbiter Deck", style "display" "flex", style "flex-direction" "column" ]
-        [ div [ style "margin" "50px", style "font-size" "50px", style "align-self" "center" ] [ text "Barendregt's Lambda Cube" ]
+        [ div [ style "margin" "50px", style "font-size" "50px", style "align-self" "center" ]
+        [ text "Barendregt's Lambda Cube" ]
         , div [ style "display" "flex", style "flex-direction" "row", style "column-gap" "200px" ]
             [ div [ style "display" "flex", style "flex-direction" "column", style "row-gap" "10px" ]
-                [ button [ onClick (SystemClicked Home  ), style "height" "55px", style "width" "150px", style "background-color" "#3c3c3c", style "color" "#ffffff", style "font-size" "30px", style "border" "0px", style "border-top-right-radius"  "20px", style "border-bottom-right-radius"  "20px" ] [ text "home"   ]
-                , button [ onClick (SystemClicked Simple), style "height" "55px", style "width" "150px", style "background-color" "#e0e0e0", style "color" "#ffffff", style "font-size" "30px", style "border" "0px", style "border-top-right-radius"  "20px", style "border-bottom-right-radius"  "20px" ] [ text "λ→"  ]
-                , button [ onClick (SystemClicked P     ), style "height" "55px", style "width" "150px", style "background-color" "#ff302d", style "color" "#ffffff", style "font-size" "30px", style "border" "0px", style "border-top-right-radius"  "20px", style "border-bottom-right-radius"  "20px" ] [ text "P"   ]
-                , button [ onClick (SystemClicked Two   ), style "height" "55px", style "width" "150px", style "background-color" "#6bff56", style "color" "#ffffff", style "font-size" "30px", style "border" "0px", style "border-top-right-radius"  "20px", style "border-bottom-right-radius"  "20px" ] [ text "2"   ]
-                , button [ onClick (SystemClicked W_    ), style "height" "55px", style "width" "150px", style "background-color" "#74c0ff", style "color" "#ffffff", style "font-size" "30px", style "border" "0px", style "border-top-right-radius"  "20px", style "border-bottom-right-radius"  "20px" ] [ text "ω_"  ]
-                , button [ onClick (SystemClicked W     ), style "height" "55px", style "width" "150px", style "background-color" "#00ffab", style "color" "#ffffff", style "font-size" "30px", style "border" "0px", style "border-top-right-radius"  "20px", style "border-bottom-right-radius"  "20px" ] [ text "ω"   ]
-                , button [ onClick (SystemClicked PW_   ), style "height" "55px", style "width" "150px", style "background-color" "#c66cc7", style "color" "#ffffff", style "font-size" "30px", style "border" "0px", style "border-top-right-radius"  "20px", style "border-bottom-right-radius"  "20px" ] [ text "Pω_" ]
-                , button [ onClick (SystemClicked P2    ), style "height" "55px", style "width" "150px", style "background-color" "#fff12e", style "color" "#ffffff", style "font-size" "30px", style "border" "0px", style "border-top-right-radius"  "20px", style "border-bottom-right-radius"  "20px" ] [ text "P2"  ]
-                , button [ onClick (SystemClicked C     ), style "height" "55px", style "width" "150px", style "background-color" "#3c3c3c", style "color" "#ffffff", style "font-size" "30px", style "border" "0px", style "border-top-right-radius"  "20px", style "border-bottom-right-radius"  "20px" ] [ text "C"   ]
+                [ button_side Home   "#3c3c3c" "home"
+                , button_side Simple "#e0e0e0" "λ→"
+                , button_side P      "#ff302d" "P"
+                , button_side Two    "#6bff56" "2"
+                , button_side W_     "#74c0ff" "ω_"
+                , button_side W      "#00ffab" "ω"
+                , button_side PW_    "#c66cc7" "Pω_"
+                , button_side P2     "#fff12e" "P2"
+                , button_side C      "#3c3c3c" "C"
                 ]
             , div [style "display" "block"]
         [ div [style "position" "absolute", style "top" "0"][WebGL.toHtml
@@ -106,22 +142,33 @@ view model =
             fragmentShader
             pointsMesh
             (uniforms model)
-        ]]]]]
+        ]]
+        , div [style "display" "flex", style "flex-direction" "row", style "position" "absolute"] 
+            [ button_trans C      "600px"  "-5px" model.gridOn
+            , button_trans W      "220px"  "20px" model.gridOn
+            , button_trans P2     "780px"  "80px" model.gridOn
+            , button_trans Two    "290px" "125px" model.gridOn
+            , button_trans PW_    "580px" "320px" model.gridOn
+            , button_trans W_     "240px" "350px" model.gridOn
+            , button_trans P      "740px" "490px" model.gridOn
+            , button_trans Simple "300px" "580px" model.gridOn
+            ]
+        ]]]
 
 
 type alias Uniforms =
-    { rotation : Mat4
+    { rotation    : Mat4
     , perspective : Mat4
-    , camera : Mat4
-    , shade : Float
+    , camera      : Mat4
+    , shade       : Float
     }
 
 uniforms : Model -> Uniforms
 uniforms {theta, eye, target} =
-    { rotation = (Mat4.makeRotate (0.05 * sin theta) (vec3 1 1 1))
+    { rotation    = Mat4.makeRotate (0.05 * sin theta) (vec3 1 1 1)
     , perspective = Mat4.makePerspective 45 1 0.01 10
-    , camera = Mat4.makeLookAt eye target (vec3 0 1 0)
-    , shade = 1
+    , camera      = Mat4.makeLookAt eye target (vec3 0 1 0)
+    , shade       = 1
     }
 
 
@@ -130,7 +177,7 @@ uniforms {theta, eye, target} =
 
 
 type alias Vertex =
-    { color : Vec3
+    { color    : Vec3
     , position : Vec3
     }
 
@@ -138,12 +185,12 @@ linesMesh : Mesh Vertex
 linesMesh =
     -- coordinates are [ left, up, towards ]
     let
-        one = vec3 0 0 1
-        two = vec3 1 0 1
+        one   = vec3 0 0 1
+        two   = vec3 1 0 1
         three = vec3 0 0 0
-        four = vec3 1 0 0
-        five = vec3 0 1 1
-        six = vec3 1 1 1
+        four  = vec3 1 0 0
+        five  = vec3 0 1 1
+        six   = vec3 1 1 1
         seven = vec3 0 1 0
         eight = vec3 1 1 0
 
